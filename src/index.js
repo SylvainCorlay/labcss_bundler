2e4066e9e37e03389cde16753dfb57130ed820df
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-05-03 14:23:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+/* @jupyterlab/cells */
+require('@jupyterlab/cells/style/collapser.css');
+require('@jupyterlab/cells/style/headerfooter.css');
+require('@jupyterlab/cells/style/inputarea.css');
+require('@jupyterlab/cells/style/widget.css');
+
+/* @jupyterlab/outputarea */
+require('@jupyterlab/outputarea/style/index.css');
+
+/* @jupyterlab/notebook */
+require('@jupyterlab/notebook/style/index.css');
+
+/* @jupyterlab/rendermime */
+require('@jupyterlab/rendermime/style/index.css');
+
+/* @jupyterlab/apputils */
+require('@jupyterlab/apputils/style/materialcolors.css');
+require('@jupyterlab/apputils/style/styling.css');
+
+/* @jupyterlab/theme-light-extension */
+require('@jupyterlab/theme-light-extension/style/variables.css');
+require('@jupyterlab/theme-light-extension/style/urls.css');
+
+/* @jupyterlab/codemirror */
+require('@jupyterlab/codemirror/style/index.css');
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ],
+  },
+};
